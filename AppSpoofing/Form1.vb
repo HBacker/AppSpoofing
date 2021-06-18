2c4ddb6e5391a69f13e403c59da7b9aa3742599f
Gökhan <38686443+HBacker@users.noreply.github.com>
2021-06-19 00:52:43 +0300

    appPath

--- AppSpoofing/Form1.vb
+++ AppSpoofing/Form1.vb
@@ -1,9 +1,17 @@
 ﻿Public Class Form1
 
     Sub arg()
-        Dim x = New ProcessStartInfo("chrome.exe")
+        Dim location As String = System.Environment.GetCommandLineArgs()(0)
+        Dim appName As String = System.IO.Path.GetFileName(location)
+        Dim app_path = AppDomain.CurrentDomain.BaseDirectory + appName.ToString
+        Dim x = New ProcessStartInfo(app_path + "chrome_origin.exe")
         x.Arguments = "-incognito"
-        Process.Start(x)
+        Try
+            Process.Start(x)
+        Catch ex As Exception
+            Console.WriteLine("Dosya Bulunamadı.")
+        End Try
+
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
